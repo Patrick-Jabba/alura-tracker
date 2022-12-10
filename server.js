3774c8659f95c4609d8b0722863359a2de490029
Patrick Monteiro Fischer <monteiromonterio@gmail.com>
2022-12-10 20:42:23 -0300

    test server

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json'); // <== Will be created later
+const router = jsonServer.router('db/db.json'); // <== Will be created later
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3200; // <== You can change the port
 
